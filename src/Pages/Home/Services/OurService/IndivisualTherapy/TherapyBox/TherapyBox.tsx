c5617a7dca334b8d9d2d8cfc541f83db8d41ed5c
Suvrodeb <suvrodevhowlader1408@gmail.com>
2025-02-26 04:38:13 +0600

    added max-w in therapy box

--- src/Pages/Home/Services/OurService/IndivisualTherapy/TherapyBox/TherapyBox.tsx
+++ src/Pages/Home/Services/OurService/IndivisualTherapy/TherapyBox/TherapyBox.tsx
@@ -13,13 +13,13 @@ const TherapyBox = ({ data }: IProps) => {
   const { title, desc, bgColor } = data;
   return (
     <div
-      className={` max-w-[172px]  h-[288px] md:max-w-[440px] md:h-[365px]  rounded-[12px] md:rounded-[24px] pt-[10px] md:pt-[48px] md:pb-[10px] px-[12px] md:px-[36px]`}
+      className={` max-w-[172px]  h-[258px] md:max-w-[440px] md:h-[365px]  rounded-[12px] md:rounded-[24px] pt-[10px] md:pt-[48px] md:pb-[10px] px-[6px] md:px-[36px]`}
       style={{ backgroundColor: bgColor }}
     >
       <h1 className="headingColor text-center font-bold text-[12px] md:text-[22px] w-[120px] md:w-[225px] mx-auto">
         {title}
       </h1>
-      <div className="w-auto md:w-[368px] text-[14px] md:text-[18px] mx-auto leading-[14px] md:leading-[34px]  text-center mt-[5px] md:mt-auto ">
+      <div className="w-[147px] md:w-[368px] text-[14px] md:text-[18px] mx-auto leading-[14px] md:leading-[34px]  text-center mt-[5px] md:mt-auto ">
         {desc}
       </div>
     </div>
